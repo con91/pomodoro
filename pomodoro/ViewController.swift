aacc9f09c9d454dc0e8a844c9f4928d5a79ea630
Conor O'Mahony <conor@ustwo.com>
2016-02-15 18:00:27 +0000

    Initial Commit

---
+++ pomodoro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pomodoro
+//
+//  Created by Conor O'Mahony on 15/02/2016.
+//  Copyright © 2016 Conor O'Mahony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
